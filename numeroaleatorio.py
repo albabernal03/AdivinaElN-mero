25046fc277de7154ab72907274632d061b00eee3
albabernal03 <ALBABERO@MYUAX.COM>
2021-11-04 19:22:45 +0100

    tarea terminada

--- numeroaleatorio.py
+++ numeroaleatorio.py
@@ -1,4 +1,16 @@
 import random
-numero = random.randint(0,100)
-print (numero)
+aleatorio = random.randrange(0,99)
+intentos = 1
+numero = int(input('Adivina el número entre 0 y 99:'))
 
+while numero != aleatorio:
+    if numero > aleatorio:
+        print ('se ha pasado!')
+    else: 
+        print('se ha quedado corto!')    
+    
+    numero = int(input('Adivine el número:'))
+    intentos +=1
+
+print ( 'Has adivinado el número:', numero)
+print ('Cantidad de intentos:', intentos)
