c1aa8ffd01dcda0dee68369bacbe7c1e712779ff
albabernal03 <ALBABERO@MYUAX.COM>
2021-11-13 10:22:01 +0100

    Creo la variable menú

--- numeroaleatorio.py
+++ numeroaleatorio.py
@@ -1,16 +1,26 @@
-import random
-aleatorio = random.randrange(0,99)
-intentos = 1
-numero = int(input('Adivina el número entre 0 y 99:'))
 
-while numero != aleatorio:
-    if numero > aleatorio:
-        print ('se ha pasado!')
-    else: 
-        print('se ha quedado corto!')    
+# import random
+# aleatorio = random.randrange(0,99)
+# intentos = 1
+# numero = int(input('Adivina el número entre 0 y 99:'))
+
+# while numero != aleatorio:
+#     if numero > aleatorio:
+#         print ('se ha pasado!')
+#     else: 
+#         print('se ha quedado corto!')    
     
-    numero = int(input('Adivine el número:'))
-    intentos +=1
+#     numero = int(input('Adivine el número:'))
+#     intentos +=1
 
-print ( 'Has adivinado el número:', numero)
-print ('Cantidad de intentos:', intentos)
+# print ( 'Has adivinado el número:', numero)
+# print ('Cantidad de intentos:', intentos)
+
+def menu():
+    print('.::ADIVINA EL NÚMERO:::')
+    print('Niveles:')
+    print('1. Del 0 al 100')
+    print('2. Del 0 al 1000')
+    print('3. Del 0 al 1.000.000')
+    print('4. Del 0 al 1.000.000.000.000')
+    
